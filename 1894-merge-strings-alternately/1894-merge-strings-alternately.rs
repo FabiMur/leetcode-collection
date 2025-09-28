3175fd64ea24038136449dbdaec375916e0c2979
Fabián J. Murgado Cáceres <fabian@fabianmurgado.com>
2025-09-28 17:00:19 +0200

    Time: 1 ms (50.58%), Space: 2.2 MB (56.94%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn merge_alternately(word1: String, word2: String) -> String {
+        word1.chars()
+            .zip(word2.chars())
+            .flat_map(|(a, b)| vec![a, b])
+            .chain(word1.chars().skip(word2.len()))
+            .chain(word2.chars().skip(word1.len()))
+            .collect()
+    }
+}
