e565812641e6e0d6b1e5e13c2b0f5faf40850842
Fabián J. Murgado Cáceres <fabian@fabianmurgado.com>
2025-10-21 18:00:53 +0200

    Time: 0 ms (100%), Space: 2.2 MB (24.15%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut m = m as usize;
+        let mut n = n as usize;
+        let mut write_index = m + n - 1;
+
+        while n > 0 {
+            if m > 0 && nums1[m - 1] > nums2[n - 1] {
+                nums1[write_index] = nums1[m - 1];
+                m -= 1;
+            } else {
+                nums1[write_index] = nums2[n - 1];
+                n -= 1;
+            }
+            write_index -= 1;
+        }
+    }
+}
