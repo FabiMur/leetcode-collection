36de7414247b77c67698b2457205cc4b7ca58013
Fabián J. Murgado Cáceres <fabian@fabianmurgado.com>
2025-10-23 15:17:36 +0200

    Time: 2 ms (33.31%), Space: 28.2 MB (28.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,14 +3,18 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int,int> freq;
 
-        for (int n: nums)
+        for (int n: nums){
             freq[n]++;
+        }
 
         int mitad = nums.size() / 2;
-        for (auto& [num, count] : freq)
-            if (count > mitad)
-                return num;
-
-        return 0;
+        for (pair<const int, int>& p : freq) {
+                int num = p.first;
+                int count = p.second;
+                if (count > mitad)
+                    return num;
+            }
+            
+        return -1;
     }
 };
