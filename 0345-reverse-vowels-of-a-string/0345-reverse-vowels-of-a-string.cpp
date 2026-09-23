d6f8e0c237f1aefcb8cb83bee9dfe143b959c4ea
Fabián J. Murgado Cáceres <fabian@fabianmurgado.com>
2026-09-23 12:02:53 +0200

    Time: 3 ms (32.35%), Space: 11.3 MB (5.58%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool isVowel(char c) {
+        return string("aeiouAEIOU").find(c) != string::npos;
+    }
+
+public:
+    string reverseVowels(string s) {
+        int left = 0;
+        int right = static_cast<int>(s.size()) - 1;
+
+        while (left < right) {
+            if (!isVowel(s[left])) {
+                ++left;
+            } else if (!isVowel(s[right])) {
+                --right;
+            } else {
+                swap(s[left], s[right]);
+                ++left;
+                --right;
+            }
+        }
+
+        return s;
+    }
+};
