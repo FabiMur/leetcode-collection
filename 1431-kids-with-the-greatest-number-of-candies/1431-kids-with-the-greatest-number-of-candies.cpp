b2772319a8ab92bc10026cd7ead156b2ac6405bf
Fabián J. Murgado Cáceres <fabian@fabianmurgado.com>
2026-09-23 10:51:10 +0200

    Time: 0 ms (100%), Space: 12.5 MB (60.14%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        int maxCandies = *max_element(candies.begin(), candies.end());
+        int max_candies = *max_element(candies.begin(), candies.end());
         vector<bool> solution;
         solution.reserve(candies.size());
 
-        for (int candy : candies) {
-            solution.push_back(candy + extraCandies >= maxCandies);
+        for(int candy: candies){
+            solution.push_back(candy + extraCandies >= max_candies);
         }
-
         return solution;
     }
 };
