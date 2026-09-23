737412318e936f66ea82f48e3f8f4a01081b22e8
Fabián J. Murgado Cáceres <fabian@fabianmurgado.com>
2026-09-23 12:04:47 +0200

    Time: 0 ms (100%), Space: 10.3 MB (47.77%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,7 +1,10 @@
 class Solution {
 private:
     static bool isVowel(char c) {
-        return string("aeiouAEIOU").find(c) != string::npos;
+        return c == 'a' || c == 'e' || c == 'i' ||
+            c == 'o' || c == 'u' || c == 'A' ||
+            c == 'E' || c == 'I' || c == 'O' ||
+            c == 'U';
     }
 
 public:
