d19fba46bc6960e6891f63ddf648c296956c5a96
Fabián J. Murgado Cáceres <fabian@fabianmurgado.com>
2026-09-23 14:00:41 +0200

    Time: 0 ms (100%), Space: 11.1 MB (32.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -5,13 +5,13 @@ public:
         vector<string> words;
         string word;
 
-        while (input >> word) {
+        while(input >> word){
             words.push_back(word);
         }
 
         string result;
-        for (int i = static_cast<int>(words.size()) - 1; i >= 0; --i) {
-            if (!result.empty()) {
+        for(int i = static_cast<int>(words.size()) - 1; i >= 0; i--){
+            if(!result.empty()){
                 result += ' ';
             }
             result += words[i];
